82028c7508e3534673e7217357c9cd1030df6693
badre zouiri <badre.rootunix@gmail.com>
2023-07-21 16:11:58 +0100

    thinks that i love

---
+++ APiTreadOrders/Controllers/NotificationController.cs
@@ -0,0 +1,19 @@
+﻿using APiTreadOrders.DAO;
+using Google.Apis.Gmail.v1.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APiTreadOrders.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        [HttpGet(Name = "GetNotificationController")]
+        public string Get()
+        {
+            Message lastEmail =GmailAcces.ReadLastEmail();
+            return "Hi Master";
+        }
+    }
+}
---
+++ APiTreadOrders/DAO/GmailAcces.cs
@@ -0,0 +1,55 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace APiTreadOrders.DAO
+{
+    public class GmailAcces
+    {
+
+        public static Message ReadLastEmail()
+        {
+            string[] scopes = { GmailService.Scope.GmailReadonly };
+
+            UserCredential credential;
+            using (var stream = new FileStream("./data/credentials2.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            // Create the Gmail API service
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "NotifierMail",
+            });
+
+            // Fetch the last email (the newest one)
+            var request = service.Users.Messages.List("me");
+            request.Q = "is:inbox"; // Only retrieve emails in the inbox
+            request.MaxResults = 1;
+            var response = request.Execute();
+            var lastEmail = response.Messages?.FirstOrDefault();
+
+            // Fetch the full email details if needed
+            if (lastEmail != null)
+            {
+                var emailRequest = service.Users.Messages.Get("me", lastEmail.Id);
+                emailRequest.Format = UsersResource.MessagesResource.GetRequest.FormatEnum.Full;
+                var emailResponse = emailRequest.Execute();
+                return emailResponse;
+            }
+
+            return null; // No emails found
+        }
+
+    }
+}
